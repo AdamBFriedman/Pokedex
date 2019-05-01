4200b2b70993c3fa363ec0592d1397765dab4ec6
Adam Friedman <49768787+AdamBFriedman@users.noreply.github.com>
2019-04-30 19:05:22 -0700

    Initial Commit

---
+++ build/precache-manifest.227635d88bbe75945f2cd30b35d2deee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0da175dc253b5b70046502c80b7695c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe0b537ae984c1791023",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e111f0223c922462d9b2",
+    "url": "/static/css/main.4e90225d.chunk.css"
+  },
+  {
+    "revision": "fe0b537ae984c1791023",
+    "url": "/static/js/2.547e4a0a.chunk.js"
+  },
+  {
+    "revision": "e111f0223c922462d9b2",
+    "url": "/static/js/main.10ce8b6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e15d418e44c1a8e4e146f06df79e61ab",
+    "url": "/static/media/oddish.e15d418e.jpg"
+  },
+  {
+    "revision": "f974386cc84d5e2e087c1c3067f763fb",
+    "url": "/static/media/spinner.f974386c.gif"
+  }
+]);
